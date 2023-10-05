242bba5907d73aae554453f3fd84761842b10ad4
ezechieldef <ezechielahouandjinou@gmail.com>
2023-10-05 11:08:47 +0100

    trois

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -373,6 +373,8 @@
 
 
 
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.kkiapay.me/k.js"></script>
     <script>
         function ancienPaiement(btn) {
