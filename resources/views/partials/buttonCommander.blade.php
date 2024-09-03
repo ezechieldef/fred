5014a5c981ccede866f7eab5cfd478c0fd3b8a8a
ezechieldef <ezechielahouandjinou@gmail.com>
2024-09-03 14:24:27 +0100

    ...

--- resources/views/partials/buttonCommander.blade.php
+++ resources/views/partials/buttonCommander.blade.php
@@ -1,2 +1,13 @@
-<a class="btn btn-warning text-white btn-lg flash-button" href="https://wa.me/22962907536">COMMANDER ET
-    TELECHARGER ICI</a>
+<a class="btn btn-success text-white btn-lg flash-button"
+    href="https://wa.me/22962907536?text=BONJOUR%20JE%20M%27APPELLE....%0AJE%20DESIRE%20ACHETER%20VOTRE%20GUIDE%20PDF%20100%20FAUTES%20COURANTES%20EN%20ANGLAIS">
+    <div class="d-flex align-items-center">
+        <i class="fa-brands fa-whatsapp me-2 h3"></i>
+        <span>
+            ACHETER ET TÉLÉCHARGER DIRECTEMENT PAR WHATSAPP ICI
+        </span>
+    </div>
+    {{-- <i class="fa-brands fa-whatsapp me-2"></i>
+    <span>
+        ACHETER ET TÉLÉCHARGER DIRECTEMENT PAR WHATSAPP ICI
+    </span> --}}
+</a>
